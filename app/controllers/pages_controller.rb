e2bfd853a480853105e7ee5051add3032ae7f0b0
Richard Gleeson <gleesonj@hotmail.com>
2018-05-09 09:11:20 +1000

    removed sqlite3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
     if user_signed_in?
       redirect_to controller: 'profiles', action: 'show'
     end
+
+    @firstsix = Product.all.order(created_at: :desc).limit(6)
   end
 
   def about
@@ -12,3 +14,4 @@ class PagesController < ApplicationController
   end
 
 end
+
