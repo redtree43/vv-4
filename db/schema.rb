01b8e8bf48e2294a1669040a8b2c8f42e32c248d
Richard Gleeson <gleesonj@hotmail.com>
2018-05-09 12:38:48 +1000

    add migrated Order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180508043026) do
+ActiveRecord::Schema.define(version: 20180509015550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "buyer_id"
+    t.bigint "user_id"
+    t.integer "order_value"
+    t.datetime "order_date"
+    t.string "delivery_address"
+    t.bigint "product_id"
+    t.integer "product_quantity"
+    t.integer "product_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.bigint "user_id"
@@ -77,6 +92,8 @@ ActiveRecord::Schema.define(version: 20180508043026) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "products"
+  add_foreign_key "orders", "users"
   add_foreign_key "products", "users"
   add_foreign_key "profiles", "users"
 end
