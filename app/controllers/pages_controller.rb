c6a6e855ee147ddb962cefb4cc6f68917e14b515
Richard Gleeson <gleesonj@hotmail.com>
2018-05-09 10:32:13 +1000

    change database to postgresql

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,9 @@
 class PagesController < ApplicationController
   def landing
-    if user_signed_in?
-      redirect_to controller: 'profiles', action: 'show'
-    end
+    # if user_signed_in?
+    #   redirect_to controller: 'profiles', action: 'show'
+    # end
+    @product = Product.find(params[user_id: 1])
 
     @firstsix = Product.all.order(created_at: :desc).limit(6)
   end
